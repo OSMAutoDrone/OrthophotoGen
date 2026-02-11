909280521694370c6b38598529cc5d51edd391f3
SimonCanelle <simon.j-bombardier@hotmail.ca>
2026-02-11 17:16:04 -0500

    basic map generation using ODM

---
+++ ODM_Orthophoto.py
@@ -0,0 +1,46 @@
+# 1. verify docker installation
+# 2. download odm image
+# 3. create command
+#      - ODM has lots of params
+# 4. run command
+
+import subprocess
+
+# result = subprocess.run(
+#     ["docker", "pull", "opendronemap/odm"],
+#     capture_output=True,
+#     text=True
+# )
+# print(result.stdout)
+
+# command= [
+#     "docker",
+#     "run", 
+#     "-ti", 
+#     "--rm", 
+#     "opendronemap/odm", 
+#     "--help",
+# ]
+
+command= [  
+    #docker params
+    "docker",
+    "run", 
+    "-ti", 
+    "--rm", 
+    "-v", "C:/Users/simon/OneDrive/Uni/PMC/code/lvl1Correction/input:/datasets",
+    "opendronemap/odm",
+    #ODM params 
+    "--project-path", "/datasets",
+    "--orthophoto-compression", "NONE", 
+    "--orthophoto-resolution", "0.01",
+    "project"
+]
+
+result = subprocess.run(
+    command,
+    capture_output=True,
+    text=True
+)
+print(result.stdout)
+print(result.stderr)
